a8218d321567f6e2fba6a9d098a71e823a848385
mohageesdiir <mgoondhe96@gmail.com>
2025-12-14 22:38:57 +0300

    complete exercise-1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dugsiiye React Exercise
+
+## 01 Exercise 1 ![](D:\dugsiiye\Dugsiiye-React-Exercise\exercise-1)
---
+++ exercise-1/src/Usercard.jsx
@@ -0,0 +1,24 @@
+
+export const UserCard = ()=>{
+    return(
+        <>
+        <div className="user">
+           {/* username  */}
+         <label htmlFor="username">
+            Username: 
+            </label>
+            <h2>Mohamad Ali</h2>
+
+            {/* email  */}
+
+            <label htmlFor="email">
+            Email:
+            </label>
+            <p>Mohamad25@gmail.com</p>
+
+            
+        </div>
+        
+        </>
+    )
+}
